7fecd3f2eece02c0a2b6f295c4daa3951cce30e1
Anirudh <anirudhrn@gmail.com>
2020-09-13 18:07:10 -0700

    Added in the basic code to calculate configurational site correlations for arbitrary crystals

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,70 @@
 #include <iostream>
 #include <casm/CASM_global_definitions.hh>
+#include <casm/clex/PrimClex.hh>
+#include <casm/app/ProjectSettings.hh>
+#include <casm/casm_io/jsonParser.hh>
+#include <casm/clex/ConfigDoF.hh>
+#include <fstream>
+#include <casm/container/Array.hh>
+#include <casm/clex/Correlation.hh>
 
-int main()
+using namespace CASM;
+using namespace std;
+/// \brief Returns unrolled per-site correlations using 'clexulator'. Supercell needs a correctly populated neighbor list.
+jsonParser correlations_mat_local(const ConfigDoF &configdof, const Supercell &scel, Clexulator &clexulator) {
+
+  int scel_vol = scel.volume();
+  int basis_size = scel.basis_size();
+  Eigen::MatrixXd correlations_mat = Eigen::MatrixXd::Zero(clexulator.corr_size(), scel_vol*basis_size);
+  clexulator.set_config_occ(configdof.occupation().begin());
+  vector<int> b_idx(scel.num_sites(),-5);
+  //Holds contribution to global correlations from a particular neighborhood
+  Eigen::VectorXd tcorr = Eigen::VectorXd::Zero(clexulator.corr_size());
+  
+  for(int v = 0; v < scel.num_sites(); v++) {
+    b_idx[v] = scel.get_b(v);
+    //Point the Clexulator to the right neighborhood
+    clexulator.set_nlist(scel.nlist().sites(v).data());
+    //Fill up contributions
+    clexulator.calc_point_corr(scel.get_b(v),tcorr.data());
+    correlations_mat.col(v) = tcorr;
+  }
+  jsonParser config_correlations;
+  config_correlations["corr_matrix"] = correlations_mat;
+  config_correlations["b"] = b_idx;
+  return config_correlations;
+}
+
+int main(int argc, char *argv[])
 {
-    /**
-     * This is where you get to shine. Place all your code in here
-     * and use the infrastructure of the repository to compile
-     * and install.
-     *
-     * If you're interested in working with something more complicated
-     * than a single main.cpp file is convenient for, check out
-     * casm-utilities!
-     * https://github.com/goirijo/casm-utilities
-     */
+    print_splash(cout);
 
-    std::cout<<"I am a sad sad executable incapable of anything"<<std::endl;
-    std::cout<<"Here is the CASM logo:"<<std::endl;
-    CASM::print_splash(std::cout);
+    if(argc!=3){
+        cerr<<"Usage:"<<argv[0]<<" <CASM project location> <output filename>"<<endl;
+        return 1;
+    }
+    
+    fs::path prim_path(argv[1]);
+    fs::path output_correlation_filename(argv[2]);
+    
+    // Load in the primclex
+    PrimClex pclex=PrimClex(fs::path(prim_path));
+    if(!pclex.has_clexulator(pclex.settings().default_clex())){
+        cout<<"You have not initialized a clexulator for this project. Please run "
+                 <<"'casm bset -u' before you run this program again."<<endl;
+        exit(666);
+    }
 
-
+    Clexulator clex = pclex.clexulator(pclex.settings().default_clex());
+    jsonParser data;
+  
+    for(auto config_iter = pclex.selected_config_cbegin(); config_iter!=pclex.selected_config_cend(); ++config_iter ){
+        auto con_name = config_iter->name();
+        cout<<"Working on : "<<con_name<<endl;
+        //Eigen::MatrixXd correlations = correlations_mat_local((*config_iter).configdof(),(*config_iter).get_supercell(),clex);
+        data[con_name] = correlations_mat_local((*config_iter).configdof(),(*config_iter).get_supercell(),clex);
+        //data["configs"][con_name]["corr_matrix"] = correlations;        
+    }
+    data.write(output_correlation_filename);
     return 0;
 }
